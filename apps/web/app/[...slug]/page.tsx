8062bdd8c72040369a97285460716f8bd8dacfa9
Jon <Jon-Lee@hy-vee.com>
2023-09-19 13:08:47 -0400

    feat: Added a 500 page.

--- apps/web/app/[...slug]/page.tsx
+++ apps/web/app/[...slug]/page.tsx
@@ -16,7 +16,6 @@ const getData = async (slug: string) => {
   const res = await fetch('http://localhost:3000/api/pages', { next: { revalidate: 10 } })
 
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
   }
 

930d5c2bfe53638436619ea88d669f6522a476b2
Jon <Jon-Lee@hy-vee.com>
2023-09-27 11:55:20 -0400

    feat: pull in CSS variables from payload global at nextjs build time, so we can utilize them in the app, in tailwind, etc

---
+++ apps/web/scripts/write-css-variables.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+const path = require('path')
+const webRoot = path.resolve(__dirname, '..');
+
+require('dotenv').config({ path: path.join(webRoot, '.env') })
+const cssFilePath = path.join(webRoot, 'app/css/variables.css');
+
+function buildCSSString(variables) {
+  let string = `:root {`
+
+  for (const name in variables) {
+    if (variables.hasOwnProperty(name)) {
+      const value = variables[name]
+
+      string += `\n\t--${name}: ${value};`
+    }
+  }
+
+  string += `\n}\n`
+
+  return string
+}
+
+function injectvariables(variables) {
+  fs.readFile(cssFilePath, 'utf8', (err, cssData) => {
+    if (err) {
+      console.error('Error reading CSS file:', err)
+      return
+    }
+
+    const cssString = buildCSSString(variables)
+
+    // Write the updated CSS content back to the file
+    fs.writeFile(cssFilePath, cssString, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing CSS file:', err)
+        return
+      }
+
+      console.log('CSS file updated successfully.')
+    })
+  })
+}
+
+async function getThemeVariables() {
+  const data = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/globals/theme-variables`)
+
+  const { values } = await data.json()
+
+  injectvariables(values)
+
+  return {}
+}
+
+getThemeVariables()

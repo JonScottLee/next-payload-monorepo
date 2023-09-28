555a3c415cdd821c3ffaaf8c70bec56be1f5b952
Jon <Jon-Lee@hy-vee.com>
2023-09-27 21:53:46 -0400

    feat: Regenerate CSS variables file when the Payload global is saved, cool.

--- apps/web/scripts/write-css-variables.js
+++ apps/web/scripts/write-css-variables.js
@@ -3,9 +3,16 @@ const path = require('path')
 const webRoot = path.resolve(__dirname, '..')
 
 require('dotenv').config({ path: path.join(webRoot, '.env') })
+
 const cssFilePath = path.join(webRoot, 'app/css/variables.css')
 
+function log(string) {
+  console.log(`[write-css-variables.js] ${string}`)
+}
+
 function buildCSSString(variables, prefix) {
+  log('Building CSS variables string...')
+
   let string = ''
 
   for (const name in variables) {
@@ -20,6 +27,8 @@ function buildCSSString(variables, prefix) {
 }
 
 function injectvariables({ colors, layout }) {
+  log('Writing CSS variables string to fil...')
+
   fs.readFile(cssFilePath, 'utf8', (err, cssData) => {
     if (err) {
       console.error('Error reading CSS file:', err)
@@ -39,12 +48,16 @@ function injectvariables({ colors, layout }) {
         return
       }
 
-      console.log('CSS file updated successfully.')
+      log('CSS file updated successfully.')
     })
   })
 }
 
+// Jon: use path.join(process.cwd(), '../web') for pathing
+
 async function getThemeVariables() {
+  log('Getting theme variables...')
+
   const data = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/globals/theme-variables`)
 
   const { colors = {}, layout = {} } = await data.json()

063570033cbdc43d26411c21d20518329d947da6
Jon <Jon-Lee@hy-vee.com>
2023-10-05 21:13:22 -0400

    feat: added richtext field

---
+++ apps/cms/src/fields/rich-text/index.ts
@@ -0,0 +1,75 @@
+import { RichTextElement, RichTextField, RichTextLeaf } from 'payload/dist/fields/config/types'
+import deepMerge from '../../utilities/deepMerge'
+import elements from './elements'
+import leaves from './leaves'
+import { link } from '../link'
+
+type RichText = (
+  overrides?: Partial<RichTextField>,
+  additions?: {
+    elements?: RichTextElement[]
+    leaves?: RichTextLeaf[]
+  }
+) => RichTextField
+
+export const richText: RichText = (
+  overrides,
+  additions = {
+    elements: [],
+    leaves: [],
+  }
+) =>
+  deepMerge<RichTextField, Partial<RichTextField>>(
+    {
+      name: 'richText',
+      type: 'richText',
+      required: true,
+      admin: {
+        upload: {
+          collections: {
+            media: {
+              fields: [
+                {
+                  type: 'richText',
+                  name: 'caption',
+                  label: 'Caption',
+                  admin: {
+                    elements: [...elements],
+                    leaves: [...leaves],
+                  },
+                },
+                {
+                  type: 'radio',
+                  name: 'alignment',
+                  label: 'Alignment',
+                  options: [
+                    {
+                      label: 'Left',
+                      value: 'left',
+                    },
+                    {
+                      label: 'Center',
+                      value: 'center',
+                    },
+                    {
+                      label: 'Right',
+                      value: 'right',
+                    },
+                  ],
+                },
+                {
+                  name: 'enableLink',
+                  type: 'checkbox',
+                  label: 'Enable Link',
+                },
+                link(),
+              ],
+            },
+          },
+        },
+        elements: [...elements, ...(additions.elements || [])],
+        leaves: [...leaves, ...(additions.leaves || [])],
+      },
+    },
+    overrides || {}
+  )

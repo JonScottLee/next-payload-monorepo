ef5cb3de0c0ca10d7d270dceccead10183fbf3a0
Jon <Jon-Lee@hy-vee.com>
2023-10-07 13:36:20 -0400

    refactor: just giving a rough header and nav, for my own sanity

--- apps/web/app/components/header/header.tsx
+++ apps/web/app/components/header/header.tsx
@@ -21,32 +21,54 @@ export const Header: FC<HeaderProps> = async ({ className }) => {
 
   return (
     <header className={`${className}`}>
-      <ul>
-        {menuData.navItems?.map((item) => {
-          const reference = item.link.reference.value as Page
+      <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <span className="font-semibold text-xl tracking-tight">Dublin Endo</span>
+        </div>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <ul className="text-sm lg:flex-grow">
+            {menuData.navItems?.map((item) => {
+              const reference = item.link.reference.value as Page
 
-          const slug = reference.slug || '/'
+              const slug = reference.slug || '/'
 
-          const isCurrentPage = slug === currentPath
+              const isCurrentPage = slug === currentPath
 
-          const ariaCurrent = isCurrentPage ? 'page' : undefined
+              const ariaCurrent = isCurrentPage ? 'page' : undefined
 
-          const classes = cx('text-brand-secondary', {
-            'underline': isCurrentPage,
-            '!text-brand-primary': isCurrentPage,
-          })
+              const classes = cx('text-brand-secondary', {
+                underline: isCurrentPage,
+                '!text-brand-primary': isCurrentPage,
+              })
 
-          return (
-            <li key={item.id}>
-              <Link legacyBehavior href={slug}>
-                <a className={classes} aria-current={ariaCurrent}>
-                  {item.link.label}
-                </a>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
+              return (
+                <li
+                  key={item.id}
+                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                >
+                  <Link legacyBehavior href={slug}>
+                    <a className={classes} aria-current={ariaCurrent}>
+                      {item.link.label}
+                    </a>
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </nav>
     </header>
   )
 }

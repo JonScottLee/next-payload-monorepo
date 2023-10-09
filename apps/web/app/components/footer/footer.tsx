75be7cc161a7c45a1a5573f800859c096b004de0
Jon <Jon-Lee@hy-vee.com>
2023-10-09 10:47:32 -0400

    refactor: basic footer, so my eyes stop bleeding.

--- apps/web/app/components/footer/footer.tsx
+++ apps/web/app/components/footer/footer.tsx
@@ -18,7 +18,7 @@ export const Footer: FC<FooterProps> = async ({ classNameName }) => {
   const footerData = await getFooterData()
 
   return (
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <footer className="bg-white dark:bg-gray-900 mt-10">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">

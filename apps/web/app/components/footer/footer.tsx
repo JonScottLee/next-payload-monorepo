8d7cb85dcbd3af5edfa43887dc934afe7fb48fa3
Jon <Jon-Lee@hy-vee.com>
2023-10-09 10:46:18 -0400

    refactor: basic footer, so my eyes stop bleeding.

--- apps/web/app/components/footer/footer.tsx
+++ apps/web/app/components/footer/footer.tsx
@@ -11,38 +11,88 @@ const getFooterData = async (): Promise<FooterType> => {
 }
 
 type FooterProps = {
-  className?: string
+  classNameName?: string
 }
 
-export const Footer: FC<FooterProps> = async ({ className }) => {
+export const Footer: FC<FooterProps> = async ({ classNameName }) => {
   const footerData = await getFooterData()
 
   return (
-    <footer className={`${className}`}>
-      {(footerData.columns || []).map((column) => {
-        return (
-          <div key={column.heading}>
-            <H2>{column.heading}</H2>
-            <nav aria-label="Site map">
-              <ul>
-                {(column.navItems || []).map((item) => {
-                  const { link } = item
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0">
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8 mr-3"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Dublin Endodontics
+            </span>
+          </a>
+          {(footerData.columns || []).map((column) => {
+            return (
+              <div key={column.heading}>
+                <H2>{column.heading}</H2>
+                <nav aria-label="Site map">
+                  <ul>
+                    {(column.navItems || []).map((item) => {
+                      const { link } = item
 
-                  const isCustomUrl = link.type === 'custom'
+                      const isCustomUrl = link.type === 'custom'
 
-                  const href = isCustomUrl ? link.url : (link.reference.value as Page).slug || '/'
+                      const href = isCustomUrl
+                        ? link.url
+                        : (link.reference.value as Page).slug || '/'
 
-                  return (
-                    <li key={item.id}>
-                      <Link href={href}>{link.label}</Link>
-                    </li>
-                  )
-                })}
-              </ul>
-            </nav>
-          </div>
-        )
-      })}
+                      return (
+                        <li key={item.id} className="mr-4 hover:underline md:mr-6">
+                          <Link href={href}>{link.label}</Link>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </nav>
+              </div>
+            )
+          })}
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{' '}
+          <a href="https://flowbite.com/" className="hover:underline">
+            Dublin Endodontics
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
     </footer>
+    // <footer classNameName={`${classNameName}`}>
+    //   {(footerData.columns || []).map((column) => {
+    //     return (
+    //       <div key={column.heading}>
+    //         <H2>{column.heading}</H2>
+    //         <nav aria-label="Site map">
+    //           <ul>
+    //             {(column.navItems || []).map((item) => {
+    //               const { link } = item
+
+    //               const isCustomUrl = link.type === 'custom'
+
+    //               const href = isCustomUrl ? link.url : (link.reference.value as Page).slug || '/'
+
+    //               return (
+    //                 <li key={item.id}>
+    //                   <Link href={href}>{link.label}</Link>
+    //                 </li>
+    //               )
+    //             })}
+    //           </ul>
+    //         </nav>
+    //       </div>
+    //     )
+    //   })}
+    // </footer>
   )
 }

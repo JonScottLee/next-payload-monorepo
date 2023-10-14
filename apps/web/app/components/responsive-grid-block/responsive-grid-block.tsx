f0ab6963b4326d81477a4199aacf59a6a05fb699
Jon <Jon-Lee@hy-vee.com>
2023-10-14 12:17:30 -0400

    feat: added trailing content to responsive grid block

--- apps/web/app/components/responsive-grid-block/responsive-grid-block.tsx
+++ apps/web/app/components/responsive-grid-block/responsive-grid-block.tsx
@@ -8,21 +8,20 @@ import classNames from 'classnames'
 export const ResponsiveGridBlock: FC<StripBlockFields<IResponsiveGrid>> = ({
   headerText,
   blocks,
+  trailingContent,
 }) => {
   const columns = blocks?.length || 0
 
   const classes = classNames('flex flex-col md:grid gap-8', {
-    'md:grid-cols-2 xl:grid-cols-4': [4, 5].indexOf(columns) > -1,
+    'md:grid-cols-2': [2, 4, 5].indexOf(columns) > -1,
     'md:grid-cols-3': columns % 3 === 0,
-    'md:grid-cols-2': columns === 2,
   })
-
-  const containerClasses = classNames('flex flex-col gap-10 md:grid');
 
   return (
     <>
       <RichText content={headerText} />
       <div className={classes}>{blocks?.map((block, i) => renderBlock(block))}</div>
+      <RichText className="mt-10" content={trailingContent} />
     </>
   )
 }
